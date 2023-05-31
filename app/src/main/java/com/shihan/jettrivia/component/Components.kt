03dbccbd33ce07e09d1e7bff41216bb6637a4e07
Kazi Ariful Islam <kazi.ariful@nexdecade.com>
2023-05-31 17:58:26 +0600

    added quiz options section

--- app/src/main/java/com/shihan/jettrivia/component/Components.kt
+++ app/src/main/java/com/shihan/jettrivia/component/Components.kt
@@ -1,18 +1,27 @@
 package com.shihan.jettrivia.component
 
 import android.util.Log
+import android.view.RoundedCorner
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -23,8 +32,11 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.shihan.jettrivia.model.Question
+import com.shihan.jettrivia.model.QuestionItem
 import com.shihan.jettrivia.screens.QuestionViewModel
 import com.shihan.jettrivia.util.AppColors
+import java.text.ChoiceFormat
 
 @Composable
 fun Questions(viewModel: QuestionViewModel) {
@@ -36,10 +48,19 @@ fun Questions(viewModel: QuestionViewModel) {
     }
 }
 
-@Preview
+
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionViewModel,
+    onNextClicked: (Int) -> Unit
+) {
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    
+    val choicesState = remember(question) {
+        question.choices?.toMutableList()
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,12 +68,61 @@ fun QuestionDisplay() {
     ) {
         Column(
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
         ) {
             
             QuestionTracker()
             DrawDottedLine(pathEffect)
+            
+            Column() {
+                Text(
+                    text = "What's the meaning of all this?",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp,
+                    color = AppColors.mOffWhite
+                )
+                
+                // choices
+                choicesState?.forEachIndexed { index, answerText ->
+                
+                
+                }
+                
+            }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun ChoiceFormat (index:Int =0, answerText:String="") {
+    Row(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .height(45.dp)
+            .border(
+                width = 4.dp, brush = Brush.linearGradient(
+                    colors = listOf(AppColors.mDarkPurple, AppColors.mOffDarkPurple)
+                ), shape = RoundedCornerShape(15.dp)
+            )
+            .clip(
+                shape = RoundedCornerShape(
+                    topStartPercent = 50,
+                    topEndPercent = 50,
+                    bottomStartPercent = 50,
+                    bottomEndPercent = 50
+                ),
+            )
+    ) {
+        Text(text = "hello", color = AppColors.mOffWhite, modifier = Modifier.padding(10.dp))
     }
 }
 
